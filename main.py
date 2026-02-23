8cc55145095425b6773312a660261477db7e1a51
AnisDerriche <anis.derriche@ynov.com>
2026-02-23 14:38:56 +0100

    ajout de la branche

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
+# | rank | name | platform | year | genre | publisher | na_sales | eu_sales | jp_sales| other_sales | global_sales |
+
 df = pd.read_csv("video_games_sales.csv")
 df.head()
+
